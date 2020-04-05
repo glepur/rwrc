a9079f2ac648519a3c94c1b0781002ebfb23a544
glepur <goran.lepur1@gmail.com>
2020-04-05 09:06:52 +0200

    use animation loop

--- client/src/graphics.rs
+++ client/src/graphics.rs
@@ -1,8 +1,10 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 use stdweb::web::html_element::CanvasElement;
 use stdweb::web::FillRule;
-use stdweb::web::{document, CanvasRenderingContext2d};
+use stdweb::web::{document, window, CanvasRenderingContext2d, RequestAnimationFrameHandle};
 
 const CENTER_RADIUS_RATIO: f64 = 0.15;
 const CENTER_COLOR: &'static str = "#333";
@@ -11,13 +13,13 @@ const POINTER_LINE_WIDTH: f64 = 8.0;
 const POINTER_COLOR: &'static str = "green";
 
 pub struct Graphics {
-  context: CanvasRenderingContext2d,
+  canvas: CanvasElement,
   center: Circle,
   pointer_circle: Circle,
   pointer_line: Line,
+  request_animation_frame_handle: Option<RequestAnimationFrameHandle>,
 }
 
-#[derive(Copy, Clone)]
 struct Circle {
   coordinates: Coordinates,
   radius: f64,
@@ -26,7 +28,6 @@ struct Circle {
   stroke_color: Option<&'static str>,
 }
 
-#[derive(Copy, Clone)]
 struct Line {
   start: Coordinates,
   end: Coordinates,
@@ -57,7 +58,6 @@ impl Graphics {
     };
 
     Graphics {
-      context: canvas.get_context().unwrap(),
       center: Circle {
         coordinates: center,
         radius: canvas.width() as f64 * CENTER_RADIUS_RATIO,
@@ -76,23 +76,28 @@ impl Graphics {
         start: center,
         end: center,
       },
+      canvas: canvas,
+      request_animation_frame_handle: None,
     }
   }
 
   pub fn draw_center(&self) {
-    self.draw_circle(self.center);
+    self.draw_circle(&self.center);
   }
 
   pub fn draw_pointer(&self) {
-    self.draw_circle(self.pointer_circle);
+    self.draw_circle(&self.pointer_circle);
     self.draw_line(self.pointer_line.start, self.pointer_line.end);
   }
 
   pub fn clear(&self) {
-    let canvas = self.context.get_canvas();
-    self
-      .context
-      .clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+    let context: CanvasRenderingContext2d = self.canvas.get_context().unwrap();
+    context.clear_rect(
+      0.0,
+      0.0,
+      self.canvas.width() as f64,
+      self.canvas.height() as f64,
+    );
   }
 
   pub fn set_touch_coordinates(&mut self, x: f64, y: f64) {
@@ -100,10 +105,18 @@ impl Graphics {
     self.pointer_circle.radius = distance(&self.pointer_line.start, &self.pointer_line.end);
   }
 
-  fn draw_circle(&self, circle: Circle) {
-    self.context.save();
-    self.context.begin_path();
-    self.context.arc(
+  pub fn animate(&mut self, rc: Rc<RefCell<Self>>) {
+    self.draw_pointer();
+    self.draw_center();
+    self.request_animation_frame_handle =
+      Some(window().request_animation_frame(move |_| rc.borrow_mut().animate(rc.clone())))
+  }
+
+  fn draw_circle(&self, circle: &Circle) {
+    let context: CanvasRenderingContext2d = self.canvas.get_context().unwrap();
+    context.save();
+    context.begin_path();
+    context.arc(
       circle.coordinates.x,
       circle.coordinates.y,
       circle.radius,
@@ -112,28 +125,29 @@ impl Graphics {
       false,
     );
     if let Some(color) = circle.fill_color {
-      self.context.set_fill_style_color(color);
-      self.context.fill(FillRule::default());
+      context.set_fill_style_color(color);
+      context.fill(FillRule::default());
     }
     if let Some(stroke_width) = circle.stroke_width {
-      self.context.set_line_width(stroke_width);
+      context.set_line_width(stroke_width);
     }
     if let Some(stroke_color) = circle.stroke_color {
-      self.context.set_stroke_style_color(stroke_color);
+      context.set_stroke_style_color(stroke_color);
     }
-    self.context.stroke();
-    self.context.restore();
+    context.stroke();
+    context.restore();
   }
 
   fn draw_line(&self, start: Coordinates, end: Coordinates) {
-    self.context.save();
-    self.context.begin_path();
-    self.context.set_line_width(POINTER_LINE_WIDTH);
-    self.context.set_stroke_style_color(POINTER_COLOR);
-    self.context.move_to(start.x, start.y);
-    self.context.line_to(end.x, end.y);
-    self.context.stroke();
-    self.context.restore();
+    let context: CanvasRenderingContext2d = self.canvas.get_context().unwrap();
+    context.save();
+    context.begin_path();
+    context.set_line_width(POINTER_LINE_WIDTH);
+    context.set_stroke_style_color(POINTER_COLOR);
+    context.move_to(start.x, start.y);
+    context.line_to(end.x, end.y);
+    context.stroke();
+    context.restore();
   }
 }
 
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,22 +1,27 @@
 mod graphics;
 
 use graphics::Graphics;
+use std::cell::RefCell;
+use std::rc::Rc;
 use stdweb::traits::*;
 use stdweb::web::event::{MouseDownEvent, MouseUpEvent, TouchMove};
 use stdweb::web::window;
 
 fn main() {
-  let mut graphics = Graphics::new();
-  graphics.draw_center();
-  graphics.draw_pointer();
+  let graphics_ref = Rc::new(RefCell::new(Graphics::new()));
+  let graphics_ref_clone = graphics_ref.clone();
+  graphics_ref.borrow().draw_center();
+  graphics_ref.borrow().draw_pointer();
 
   window().add_event_listener(move |event: TouchMove| {
     let touch = &event.touches()[0];
-    graphics.clear();
-    graphics.set_touch_coordinates(touch.client_x() as f64, touch.client_y() as f64);
-    graphics.draw_pointer();
-    graphics.draw_center();
+    graphics_ref_clone.borrow().clear();
+    graphics_ref_clone
+      .borrow_mut()
+      .set_touch_coordinates(touch.client_x() as f64, touch.client_y() as f64);
   });
+
+  graphics_ref.borrow_mut().animate(graphics_ref.clone());
 
   stdweb::event_loop();
 }
